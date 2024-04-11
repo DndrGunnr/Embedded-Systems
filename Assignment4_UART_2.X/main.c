96cd0ac6a959900dc203008bc7a9070e5ee774ea
Luca Cornia <97911589+Luca23C@users.noreply.github.com>
2024-04-11 14:54:16 +0200

    Disable UART_setup function

--- Assignment4_UART_2.X/main.c
+++ Assignment4_UART_2.X/main.c
@@ -32,6 +32,20 @@ void algorithm() {
 }
 
 int main() {
+    // Setup UART
+    //pin mapping
+    //mapping of remappable pin RP64 on pin RD0 to work as UART1TX
+    RPOR0bits.RP64R = 1; 
+    //mapping of remappable pin RPI75 on pin RD11 to work as UA1RX
+    RPINR18bits.U1RXR = 0x4b; // 0x4b is 75 in hexadecimal
+    //UART1 init
+    //BaudRate selection
+    //U1BRG = (FCY/(16*baud))-1; //(72MHz/(16*9600))-1
+    U1BRG = 468;
+    U1MODEbits.UARTEN = 1; //global UART enable
+    U1STAbits.UTXEN = 1; //UART1 enable
+    IEC0bits.U1RXIE = 1; //UART1 interrupt enable
+    
     int ret;
     //LED2 as output
     TRISAbits.TRISA0 = 0;
@@ -43,7 +57,7 @@ int main() {
     // activate enable for interrupts
     IEC0bits.T3IE = 1;
     // Setup UART
-    uart_setup(9600);
+    //uart_setup(9600);
     
     while(1) {
         algorithm();
