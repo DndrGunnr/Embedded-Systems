137ca1b27bffd7dd3e54c148a9a658be0eacf9ef
SimoneLombardi <146358714+SimoneLombardi@users.noreply.github.com>
2024-05-09 22:57:32 +0200

    commit 2, info sul codice

--- TEST_interrupts_uart.X/main.c
+++ TEST_interrupts_uart.X/main.c
@@ -12,14 +12,18 @@
 #include "stdio.h"
 #include "string.h"
 
-int16_t toSend_char; // varaibile di conteggio per sapere quanti char mandare
+int16_t toSend_char = 0; // varaibile di conteggio per sapere quanti char mandare
 
 
 void __attribute__((__interrupt__, no_auto_psv__))_U1TXInterrupt(void) {
     IFS0bits.U1TXIF = 0; // flag to zero
+    LATGbits.LATG9 = (!LATGbits.LATG9);// led flik to se interr action
     
     uart_send_head();
     uart_buff_rmv();
+    toSend_char = toSend_char - 1;
+    
+    IEC0bits.U1TXIE = 0; // dopo aver inviato un elemento spengo interrupt
 }
 
 void __attribute__((__interrupt__, no_auto_psv__))_U1RXInterrupt(void) {
@@ -27,6 +31,8 @@ void __attribute__((__interrupt__, no_auto_psv__))_U1RXInterrupt(void) {
     
     // carattere ricevuto messo nel buffer
     uart_buff_add();
+    // incremento valore di toSend_char;
+    toSend_char = toSend_char + 1;
 }
 
 
@@ -42,8 +48,8 @@ int main(void) {
     TRISAbits.TRISA0 = 0;
     
     while(1){
-        if(uart_is_buff_empty()){
-            LATGbits.LATG9 = 1; // accendo led
+        if(toSend_char == 0){
+             // accendo led
             // se il buffer è vuoto disattivo l'interrupt di invio
         }else{
             IEC0bits.U1TXIE = 1;
@@ -53,3 +59,13 @@ int main(void) {
     
     return 0;
 }
+
+// COMMIT 1 COMPORTAMENTO:
+    /*inizia con comportamento corretto, dopo aver inviato qualche carattere comincia a 
+     stampare 2 caratteri per volta (caratteri extra sembrano essere in un ciclo con 
+     * caratteri precedenti) err__if buff_empty settava ENABLE TX a zero*/
+
+// COMMIT 2 COMPORTAMENTO:
+    
+
+
