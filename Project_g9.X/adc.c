1ca2a5399ca31327d99c8ca1b8bcfa88fdcb2479
Luca Cornia <97911589+Luca23C@users.noreply.github.com>
2024-06-18 18:28:20 +0200

    commit for merging

--- Project_g9.X/adc.c
+++ Project_g9.X/adc.c
@@ -21,19 +21,19 @@ void adc_setup(){
     AD1CON2bits.CHPS = 0; // Channel number selection
     AD1CHS0bits.CH0NA = 0;// Channel 0 negative input is VREFL
     
-    ANSELBbits.ANSB14 = 1;// analog pin mode selection
+    ANSELBbits.ANSB15 = 1;// analog pin mode selection
     ANSELBbits.ANSB11 = 1;// activate AN14 AN11
     
-    // dal file di dave+annika
-    TRISBbits.TRISB11 = 1;
-    TRISBbits.TRISB14 = 1;
     
-    TRISBbits.TRISB9 = 0;
-    LATBbits.LATB9 = 1;
-    // dal file di dave+annika
+    TRISBbits.TRISB11 = 1;
+    TRISBbits.TRISB15 = 1;
+    
+    TRISAbits.TRISA3 = 0;
+    LATAbits.LATA3 = 1;
+    
 
     AD1CSSLbits.CSS11 = 1;// insert AN14 AN11 in the scan sequence
-    AD1CSSLbits.CSS14  = 1;
+    AD1CSSLbits.CSS15  = 1;
 
     AD1CON2bits.SMPI = 1;// n of sequential operation before interrupt (if sequential is set)
     
