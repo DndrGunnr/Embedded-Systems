6f05fcd80dbb840a1d49d02adbbd2dadcbb43677
Enrico Dondero <enrico.dondero.p@gmail.com>
2024-06-19 02:16:56 +0200

    Progetto nuber

--- Project_g9.X/uart.h
+++ Project_g9.X/uart.h
@@ -37,6 +37,8 @@
 
 #define RX_DIM 200
 #define TX_DIM 200
+#define TX 1
+#define RX 2
 #define AK_DIM 8 
 #define HD_DIM 7
 #define COMM_GOOD 1
@@ -44,6 +46,7 @@
 #define BATTERY   3
 #define IR        4
 #define MSG_END   5
+#define MAX_COMMAND 10
 
 typedef struct{
     int head;
@@ -56,28 +59,27 @@ int16_t uart_setup(int16_t TX_interrupt_on, int16_t TX_interrupt_type, int16_t R
 
 void save_char(char carattere);
 
-int16_t command_empty();
+int16_t buffer_empty(int mode);
 
 void discard_command();
 
 char get_char();
 
-void move_command_head();
-
 int16_t parse_payload(char *payload, int16_t payload_dim);
 
-int16_t get_command_tail();
-
-int16_t get_data_nuber();
+int16_t get_buffer_length(buffer*);
 
 void append_responce(int16_t type);
 void append_number(double value, int16_t type);
 
-int16_t get_responce_head();
-char *get_responce();
-void move_responce_head();
-int16_t responce_empty();
+int16_t get_buffer_head(int);
+char *get_buffer(int);
+void move_buffer_head(int);
 
-// --------------------------------------------------------------- DEBUG MOMENTANEO ---------------------------------------------------------------//
-void print_buff_log();
-void print_comm_log(int16_t x, int16_t t);
+
+void buffer_init(buffer*);
+
+
+void print_buff_log(int mode);
+
+int16_t get_data_nuber();
